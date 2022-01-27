48f4214aa8f26733fde77056ae332b1350dc78c7
Amber W <43912805+Ambear-W@users.noreply.github.com>
2022-01-26 21:33:59 -0500

    Discord bot set up
    Setting up the bot and getting the bases of how the commands work.  Will work on getting Firebase figured out!

---
+++ botSetUp.py
@@ -0,0 +1,61 @@
+import hikari
+import lightbulb
+
+#make sure you activate the evn/virtual enviroment to test: venv & then run the python code py botSetUp.py
+
+#Note, every time you make changes, close the bot in the terminal and then restart it.  It will then have your changes.
+
+#basic auto-sharding bot implementation, uses the token to connect to your bot
+#don't share this (aka AMBER MAKE SURE YOU REMOVE IT BEFORE YOU PUT IT ON GITHUB SINCE IT'S PUBLIC)
+bot = lightbulb.BotApp(
+    token='', 
+    default_enabled_guilds=()
+)
+
+#The below is an example of an eventlistener!  eventlistners are similar to functions, expect they are listening to something!
+#  the code that will run when ever the event is triggered, which is every time a message is sent/created in the channel
+#   it will print what was sent in the channel in the terminal/console
+#@bot.listen(hikari.GuildMessageCreateEvent)
+#async def print_message(event):
+    #print(event.content)
+
+
+#These are the commands!  These are the things that your bot will do! (EX: !ping the bot will replay with Pong!)
+@bot.command
+#name of command, description of command
+@lightbulb.command('ping', 'Says pong!')
+#this is a slash command - when creating slash commands (type / in discord chanell), 
+#TAKE NOTICE- discord takes some time regristing the commands, its easiest to copy the id of your server and add them to the token link.
+@lightbulb.implements(lightbulb.SlashCommand)
+async def ping(ctx):
+    await ctx.respond('Pong!')
+
+#There are groups and sub commands as well, they are very similar to the previous commands - but there are differences
+@bot.command
+@lightbulb.command('group', 'This is a group')
+#this command is going to have subcommands and will be grouped toogether
+@lightbulb.implements(lightbulb.SlashCommandGroup)
+async def my_group(ctx):
+    #do nothing just go on!
+    #why? --> group commands cannot be run with just slash commands
+    pass
+
+#we want the subcommands to be a child of the group we made
+@my_group.child
+@lightbulb.command('subcommand', 'This is a subcommand')
+@lightbulb.implements(lightbulb.SlashSubCommand)
+async def subcommand(ctx):
+    await ctx.respond('I am a subcommand!')
+
+#we can add options to our commands but adding decorations! 
+@bot.command
+#here we add the options - you dont have to do type but it'll automatically make it a string
+#the options HAVE to be ABOVE the lightbulb.command and UNDER bot.command
+@lightbulb.option('num2', 'The second number', type=int)
+@lightbulb.option('num1', 'The first number', type=int)
+@lightbulb.command('add', 'Add two numbers together')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def add(ctx):
+    await ctx.respond(ctx.options.num1 + ctx.options.num2)
+
+bot.run()
